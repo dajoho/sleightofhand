db54476b72a3fed1c0e96093d9ecdd29cfdc6b38
Dave Holloway <dh@dajoho.com>
2010-12-15 22:30:06 +0000

    support for sallycms.
    git-svn-id: http://svn.daveholloway.co.uk/sleightofhand/trunk@21 1350f556-3d3a-4425-922c-00063309e4d5

--- install.inc.php
+++ install.inc.php
@@ -2,18 +2,24 @@
 
 $install = true;
 
-
-$folder = 'files/soh/';
-if (!file_exists($REX['HTDOCS_PATH'].$folder)) {
-	if (!mkdir($REX['HTDOCS_PATH'].$folder)) {
-		$REX['ADDON']['installmsg']['sleightofhand'] = 'Please check permissions on: '.$folder;
-		$install = false;
+if (!defined('IS_SALLY')) {
+	$folder = 'files/soh/';
+	if (!file_exists($REX['HTDOCS_PATH'].$folder)) {
+		if (!mkdir($REX['HTDOCS_PATH'].$folder)) {
+			$REX['ADDON']['installmsg']['sleightofhand'] = 'Please check permissions on: '.$folder;
+			$install = false;
+		}
 	}
+
+} else {
+	$service = sly_Service_Factory::getService('AddOn');
+	$pubDir  = $service->publicFolder('sleightofhand');
+	//$state   = rex_is_writable($pubDir);
+	//if ($state !== true) {
+	//	throw new Exception($state);
+	//}
 }
 
-
-
 $REX['ADDON']['install']['sleightofhand'] = 1;
-
 
 ?>
