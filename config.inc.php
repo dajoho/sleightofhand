4c1c3334181981e7c9e6e5aaf01fbd4833ccf2fd
Dave Holloway <dh@dajoho.de>
2012-03-11 20:12:37 +0100

    First steps to Zend Style

--- config.inc.php
+++ config.inc.php
@@ -7,36 +7,42 @@ $REX['ADDON']['page'][$mypage] = $mypage;
 $REX['ADDON']['version'][$mypage] = "1.0";
 $REX['ADDON']['author'][$mypage] = "Dave Holloway";
 $REX['ADDON']['settings'][$mypage]['replacements'] = array();
-$REX['ADDON']['settings'][$mypage]['imagemagic']='';
+$REX['ADDON']['settings'][$mypage]['imagemagic'] = '';
 
 // classes, functions
-require_once $REX['INCLUDE_PATH']. '/addons/sleightofhand/classes/class.sleightofhand.inc.php';
-require_once $REX['INCLUDE_PATH']. '/addons/sleightofhand/classes/class.magic.inc.php';
-require_once $REX['INCLUDE_PATH']. '/addons/sleightofhand/functions/function.sleightofhand.inc.php';
+require_once $REX['INCLUDE_PATH']
+        . '/addons/sleightofhand/classes/class.sleightofhand.inc.php';
+require_once $REX['INCLUDE_PATH']
+        . '/addons/sleightofhand/classes/class.magic.inc.php';
+require_once $REX['INCLUDE_PATH']
+        . '/addons/sleightofhand/functions/function.sleightofhand.inc.php';
 
 // extension points
-require_once $REX['INCLUDE_PATH']. '/addons/sleightofhand/extensions/extension.scripts.inc.php';
-require_once $REX['INCLUDE_PATH']. '/addons/sleightofhand/extensions/extension.cache.inc.php';
+require_once $REX['INCLUDE_PATH']
+        . '/addons/sleightofhand/extensions/extension.scripts.inc.php';
+require_once $REX['INCLUDE_PATH']
+        . '/addons/sleightofhand/extensions/extension.cache.inc.php';
 
 // include phpquery + replacement EP, if using php5
 if (version_compare(PHP_VERSION, '5.0.0', '>')) {
     if (!class_exists('phpQuery')) {
-    require_once $REX['INCLUDE_PATH']. '/addons/sleightofhand/classes/class.phpquery.inc.php';
-    require_once $REX['INCLUDE_PATH']. '/addons/sleightofhand/extensions/extension.replacements.inc.php';
+        require_once $REX['INCLUDE_PATH']
+                . '/addons/sleightofhand/classes/class.phpquery.inc.php';
+        require_once $REX['INCLUDE_PATH']
+                . '/addons/sleightofhand/extensions/extension.replacements.inc.php';
     }
 }
-
 
 // ---------------------------------------------------------------------------
 // Special requests
 # retrieve an image if it is requested
-$soh = rex_request('a561_soh','string');
-if ($soh!="") {
-    $soh = str_replace('/','',$soh);
-    $soh = str_replace('.','',$soh);
-    $soh = str_replace("\\",'',$soh);
+$soh = rex_request('a561_soh', 'string');
+if ($soh != "") {
+    $soh = str_replace('/', '', $soh);
+    $soh = str_replace('.', '', $soh);
+    $soh = str_replace("\\", '', $soh);
 
-    $cachefile = $REX['INCLUDE_PATH'].'/generated/files/soh-'.$soh.'.png';
+    $cachefile = $REX['INCLUDE_PATH'] . '/generated/files/soh-' . $soh . '.png';
     if (file_exists($cachefile)) {
         header("Content-type: image/png");
         $fp = fopen($cachefile, 'rb');
@@ -47,18 +53,17 @@ if ($soh!="") {
 }
 # css, javascripts etc.
 if (isset($_REQUEST['a561_css'])) {
-    $cssfile = $REX['INCLUDE_PATH'] .'/addons/sleightofhand/data/soh.css';
+    $cssfile = $REX['INCLUDE_PATH'] . '/addons/sleightofhand/data/soh.css';
     rex_send_file($cssfile, 'text/css');
     exit();
 }
 if (isset($_REQUEST['a561_js'])) {
-    $jsfile = $REX['INCLUDE_PATH'] .'/addons/sleightofhand/data/soh.js';
+    $jsfile = $REX['INCLUDE_PATH'] . '/addons/sleightofhand/data/soh.js';
     rex_send_file($jsfile, 'text/javascript');
     exit();
 }
 if (isset($_REQUEST['a561_js-ie'])) {
-    $jsfile = $REX['INCLUDE_PATH'] .'/addons/sleightofhand/data/soh-ie.js';
+    $jsfile = $REX['INCLUDE_PATH'] . '/addons/sleightofhand/data/soh-ie.js';
     rex_send_file($jsfile, 'text/javascript');
     exit();
 }
-?>
--- extensions/extension.replacements.inc.php
+++ extensions/extension.replacements.inc.php
@@ -1,33 +1,35 @@
 <?php
-function a561_replacements($params) {
+function a561_replacements($params)
+{
     global $REX;
     $output = $params['subject'];
-    
+
     $reps = $REX['ADDON']['settings']['sleightofhand']['replacements'];
-    
-    if (count($reps)>0) {
+
+    if (count($reps) > 0) {
         $doc = phpQuery::newDocument($output);
         foreach ($reps as $rep) {
-            if (count($rep)==2) {
+            if (count($rep) == 2) {
                 $elems = pq($rep[0]);
-                foreach($elems as $elem) {
+                foreach ($elems as $elem) {
                     $text = pq($elem)->html();
                     $text = strip_tags($text);
                     $settings = $rep[1];
-                    $settings['text']=$text;
+                    $settings['text'] = $text;
                     $html = a561_sleightofhand($settings);
                     pq($elem)->html($html);
                 }
-                
+
             }
         }
         $output = pq($doc)->markup();
-        
+
         //remove empty tags
-        $output = preg_replace('/<(p|span|strong|b|em|h1|h2|h3|h4|h5|h6)>(\s|\b)*<\/\1>/','',$output);
+        $output = preg_replace(
+                '/<(p|span|strong|b|em|h1|h2|h3|h4|h5|h6)>(\s|\b)*<\/\1>/', '',
+                $output);
     }
-    
+
     return $output;
 }
-rex_register_extension('OUTPUT_FILTER', 'a561_replacements'); 
-?>
+rex_register_extension('OUTPUT_FILTER', 'a561_replacements');
