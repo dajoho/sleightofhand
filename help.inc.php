8a8fd43163c1ba64917cf181802a9e381c7bf24b
Dave Holloway <dh@dajoho.com>
2012-03-25 22:19:19 +0200

    Swapped from GNU to MIT. Code is no longer dependent on REDAXO core.

--- help.inc.php
+++ help.inc.php
@@ -7,7 +7,7 @@
  * @category Sleightofhand
  * @package  Sleightofhand
  * @author   Dave Holloway <dh@dajoho.de>
- * @license  GNU http://www.gnu.org/licenses/gpl-2.0.html
+ * @license  MIT License http://www.opensource.org/licenses/mit-license.html
  * @version  GIT: <git_id>
  * @link     http://bit.ly/sleightofhand-site
  */
