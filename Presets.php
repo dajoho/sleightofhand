93a398f6d09a327d8b11aec06c42a09b3ec90f6b
Heiko Adams <ha@gn2-netwerk.de>
2013-03-08 16:40:41 +0100

    Support für Presets SOH-1

---
+++ Presets.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Array containing the preset definition(s)
+ *
+ * Every preset has at least to provide informations about
+ * - font
+ * - size
+ * - color
+ * */
+
+Sleightofhand_presets::$default_presets = array(
+    'default'   => array('font'=>'Korean_Calligraphy.ttf',
+        /*'fontpath'=>'',*/
+        'size'=>'24',
+        'color'=>'#8B0000'/*,
+        'wordwrap'=>'',
+        'quality'=>'',
+        'text-align'=>''*/));
---
+++ classes/Presets/Presets.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: heiko
+ * Date: 08.03.13
+ * Time: 11:50
+ * To change this template use File | Settings | File Templates.
+ */
+class Sleightofhand_presets
+{
+    public static $default_presets = array();
+}
